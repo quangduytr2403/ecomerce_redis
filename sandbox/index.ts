922c5d530d11df5eea22f70a3e33db60e04c6e31
Trần Quang Duy <quangduytr97@gmail.com>
2026-03-10 07:48:35 +0700

    implement getItems by pipeline

--- sandbox/index.ts
+++ sandbox/index.ts
@@ -2,12 +2,25 @@ import 'dotenv/config';
 import { client } from '../src/services/redis';
 
 const run = async () => {
-    await client.hSet('car', {
+    await client.hSet('car1', {
         color: 'red',
         year: 1950,
     });
+    await client.hSet('car2', {
+        color: 'blue',
+        year: 1960,
+    });
+    await client.hSet('car3', {
+        color: 'green',
+        year: 1970,
+    });
 
-    const car = await client.hGetAll('cara');
-    console.log(car);
+    const commands = [1, 2, 3].map((id) => {
+        return client.hGetAll(`car${id}`);
+    })
+
+    const results = await Promise.all(commands);
+
+    console.log(results);
 };
 run();
