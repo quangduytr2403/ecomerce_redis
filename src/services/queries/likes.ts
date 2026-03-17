c455c502dfe5f4822d40c72c2f8f9d5a48c9eabd
Trần Quang Duy <quangduytr97@gmail.com>
2026-03-17 08:31:48 +0700

    implement likes and user check

--- src/services/queries/likes.ts
+++ src/services/queries/likes.ts
@@ -1,9 +1,35 @@
-export const userLikesItem = async (itemId: string, userId: string) => {};
+import { client } from "$services/redis";
+import { getItems } from "$services/queries/items";
+import { userLikesKey, itemsKey } from "$services/keys";
 
-export const likedItems = async (userId: string) => {};
+export const userLikesItem = async (itemId: string, userId: string) => {
+    return client.sIsMember(userLikesKey(userId), itemId);
+};
 
-export const likeItem = async (itemId: string, userId: string) => {};
+export const likedItems = async (userId: string) => {
+    const likedItemIds = await client.sMembers(userLikesKey(userId));
 
-export const unlikeItem = async (itemId: string, userId: string) => {};
+    return getItems(likedItemIds);
+};
 
-export const commonLikedItems = async (userOneId: string, userTwoId: string) => {};
+export const likeItem = async (itemId: string, userId: string) => {
+    const inserted = await client.sAdd(userLikesKey(userId), itemId);
+
+    if (inserted) {
+        return await client.hIncrBy(itemsKey(itemId), 'likes', 1);
+    }
+};
+
+export const unlikeItem = async (itemId: string, userId: string) => {
+    const removed = await client.sRem(userLikesKey(userId), itemId);
+
+    if (removed) {
+        return await client.hIncrBy(itemsKey(itemId), 'likes', -1);
+    }
+};
+
+export const commonLikedItems = async (userOneId: string, userTwoId: string) => {
+    const intersection = await client.sInter([userLikesKey(userOneId), userLikesKey(userTwoId)]);
+
+    return getItems(intersection);
+};
--- src/services/queries/users.ts
+++ src/services/queries/users.ts
@@ -1,6 +1,6 @@
 import type { CreateUserAttrs } from '$services/types';
 import { genId } from '$services/utils';
-import { usersKey } from '$services/keys';
+import { usersKey, usernamesUniqueKey } from '$services/keys';
 import { client } from '$services/redis';
 
 export const getUserByUsername = async (username: string) => { };
@@ -13,7 +13,14 @@ export const getUserById = async (id: string) => {
 export const createUser = async (attrs: CreateUserAttrs) => {
     const id = genId();
     const key = usersKey(id);
+    const exists = await client.sIsMember(usernamesUniqueKey(), attrs.username);
+
+    if (exists) {
+        throw new Error('Username already exists');
+    }
+
     await client.hSet(key, serialize(attrs));
+    await client.sAdd(usernamesUniqueKey(), attrs.username);
     return id;
 };
 
